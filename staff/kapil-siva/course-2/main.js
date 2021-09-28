0ec5f4c828c8c697395dc21e26a1aa4790a93002
kapil <kapil.siva@hotmail.com>
2021-09-28 14:28:57 +0200

    first js work

---
+++ staff/kapil-siva/course-2/main.js
@@ -0,0 +1,23 @@
+console.log("Starting javascript");
+var myName = "Kapilraj"
+console.log(myName);
+var age = 35;
+var  ignasiAge = 20;
+console.log(age);
+let ageDiff = age - ignasiAge;
+console.log(ageDiff);
+
+if (age >= 21) {
+    console.log("You are older than 21");
+} else {
+    console.log("You are not older that 21");
+}
+if (age === ignasiAge) {
+    console.log("You have the same age as Ignasi");
+} else if (age < ignasiAge){
+    console.log("Ignasi is older than you")
+} else {
+    console.log("Ignasi is younger than you");
+}
+
+
