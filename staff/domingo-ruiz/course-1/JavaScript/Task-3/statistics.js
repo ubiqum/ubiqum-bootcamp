566cad4b269ba1d0d74fd9711613f2de700dae87
Domin1820 <domin1820@gmail.com>
2021-10-14 17:56:01 +0200

    add functions and tables

--- staff/domingo-ruiz/course-1/JavaScript/Task-3/statistics.js
+++ staff/domingo-ruiz/course-1/JavaScript/Task-3/statistics.js
@@ -5063,9 +5063,9 @@ const data =
 
 
 //task-3
+var senatArr = data.results[0].members;
 
 //number of democrats, republicans and independents. 2 ways to find the number of members of every party
-var senatArr = data.results[0].members;
                             ////////
 // var partyMembers = [];
 
@@ -5150,168 +5150,158 @@ document.getElementById("idPcn").textContent = pcnVotedI;
 
 
 
-
-    // 10 lowest percents
-
+// Most and less engaged
 var loyaltyMembers = [...senatArr];
 
-
-loyaltyMembers.sort(function (x,y){return x.votes_with_party_pct - y.votes_with_party_pct; });
+loyaltyMembers.sort(function (x,y){return y.missed_votes - x.missed_votes; });
  console.log(loyaltyMembers)
 
 var l = (loyaltyMembers.length*0.1);
-var low10 = loyaltyMembers.slice(0,l);
+var attLow10 = loyaltyMembers.slice(0,l);
 
 
-console.log(low10);
 
   
 
-loyaltyMembers.sort(function (x,y){return y.votes_with_party_pct - x.votes_with_party_pct; });
+loyaltyMembers.sort(function (x,y){return x.missed_votes - y.missed_votes; });
  console.log(loyaltyMembers)
 
 var l = (loyaltyMembers.length*0.1);
-var high10 = loyaltyMembers.slice(0,l);
+var attHigh10 = loyaltyMembers.slice(0,l);
 
 
-console.log(high10);
-
-
-
+console.log(attHigh10);
 
 
 
 
-//Bottom 10 loyalty table
- 
- function buildTable(low10) {
-
-  let table = document.getElementById("bottom10");
-  let tblBody = document.createElement("tbody");
-  let thead = document.createElement("thead");
-//HEADER
-    let th = document.createElement("th");
-    let th2 = document.createElement("th");
-    let th3 = document.createElement("th");
-
-
-    let textHeader = document.createTextNode("Name");
-    let textHeader2 = document.createTextNode("No of Missed Votes");
-    let textHeader3 = document.createTextNode("& of Missed Votes");
-
-    th.appendChild(textHeader);
-    th2.appendChild(textHeader2);
-    th3.appendChild(textHeader3);
-
-    thead.appendChild(th)
-    thead.appendChild(th2)
-    thead.appendChild(th3)
 
 
 
- for (var i = 0; i < low10.length; i++) {
-  let link = document.createElement("a");
-  link.setAttribute("href", low10[i].url)
 
-  let tr = document.createElement("tr")
-  let td1 = document.createElement("td");
-  let td2 = document.createElement("td");
-  let td3 = document.createElement("td");
 
-  let text1 = document.createTextNode(low10[i].first_name + " " + (low10[i].middle_name || "") + " " + low10[i].last_name);
-  link.appendChild(text1);
+//TABLES
 
-  let text2 = document.createTextNode(low10[i].missed_votes);
-  let text3 = document.createTextNode(low10[i].missed_votes_pct);
 
+function buildTable3(attLow10) {
+
+    let table = document.getElementById("attlow10");
+    let tblBody = document.createElement("tbody");
+    let thead = document.createElement("thead");
+  //HEADER
+      let th = document.createElement("th");
+      let th2 = document.createElement("th");
+      let th3 = document.createElement("th");
   
-  td1.appendChild(link);
-  td2.appendChild(text2);
-  td3.appendChild(text3);
-
   
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  tr.appendChild(td3);
-
-
+      let textHeader = document.createTextNode("Name");
+      let textHeader2 = document.createTextNode("No of Missed Votes");
+      let textHeader3 = document.createTextNode("& of Missed Votes");
+  
+      th.appendChild(textHeader);
+      th2.appendChild(textHeader2);
+      th3.appendChild(textHeader3);
+  
+      thead.appendChild(th)
+      thead.appendChild(th2)
+      thead.appendChild(th3)
+  
+  
+  
+   for (var i = 0; i < attLow10.length; i++) {
+    let link = document.createElement("a");
+    link.setAttribute("href", attLow10[i].url)
+  
+    let tr = document.createElement("tr")
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+  
+    let text1 = document.createTextNode(attLow10[i].first_name + " " + (attLow10[i].middle_name || "") + " " + attLow10[i].last_name);
+    link.appendChild(text1);
+  
+    let text2 = document.createTextNode(attLow10[i].missed_votes);
+    let text3 = document.createTextNode(attLow10[i].missed_votes_pct);
+  
+    
+    td1.appendChild(link);
+    td2.appendChild(text2);
+    td3.appendChild(text3);
+  
+    
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+  
+  
   tblBody.appendChild(tr);
-
-  }
-
-  table.appendChild(thead);
-  table.appendChild(tblBody);
-}
-
-buildTable(low10);
-
-
-
-
-
-
-
-//top 10 loyalty table
-
-function buildTable2(high10) {
-
-  let table = document.getElementById("higher10");
-  let tblBody = document.createElement("tbody");
-  let thead = document.createElement("thead");
-//HEADER
-    let th = document.createElement("th");
-    let th2 = document.createElement("th");
-    let th3 = document.createElement("th");
-
-
-    let textHeader = document.createTextNode("Name");
-    let textHeader2 = document.createTextNode("No of Missed Votes");
-    let textHeader3 = document.createTextNode("& of Missed Votes");
-
-    th.appendChild(textHeader);
-    th2.appendChild(textHeader2);
-    th3.appendChild(textHeader3);
-
-    thead.appendChild(th)
-    thead.appendChild(th2)
-    thead.appendChild(th3)
-
-
-
- for (var i = 0; i < high10.length; i++) {
-  let link = document.createElement("a");
-  link.setAttribute("href", high10[i].url)
-
-  let tr = document.createElement("tr")
-  let td1 = document.createElement("td");
-  let td2 = document.createElement("td");
-  let td3 = document.createElement("td");
-
-  let text1 = document.createTextNode(high10[i].first_name + " " + (high10[i].middle_name || "") + " " + high10[i].last_name);
-  link.appendChild(text1);
-
-  let text2 = document.createTextNode(high10[i].missed_votes);
-  let text3 = document.createTextNode(high10[i].missed_votes_pct);
-
   
-  td1.appendChild(link);
-  td2.appendChild(text2);
-  td3.appendChild(text3);
-
+    }
   
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  tr.appendChild(td3);
-
-
-tblBody.appendChild(tr);
-
+    table.appendChild(thead);
+    table.appendChild(tblBody);
   }
-
-  table.appendChild(thead);
-  table.appendChild(tblBody);
-}
-
-buildTable2(high10);
-
-
+  
+  buildTable3(attLow10);
+  
+  function buildTable4(attHigh10) {
+  
+    let table = document.getElementById("atthigh10");
+    let tblBody = document.createElement("tbody");
+    let thead = document.createElement("thead");
+  //HEADER
+      let th = document.createElement("th");
+      let th2 = document.createElement("th");
+      let th3 = document.createElement("th");
+  
+  
+      let textHeader = document.createTextNode("Name");
+      let textHeader2 = document.createTextNode("No of Missed Votes");
+      let textHeader3 = document.createTextNode("& of Missed Votes");
+  
+      th.appendChild(textHeader);
+      th2.appendChild(textHeader2);
+      th3.appendChild(textHeader3);
+  
+      thead.appendChild(th)
+      thead.appendChild(th2)
+      thead.appendChild(th3)
+  
+  
+  
+   for (var i = 0; i < attHigh10.length; i++) {
+    let link = document.createElement("a");
+    link.setAttribute("href", attHigh10[i].url)
+  
+    let tr = document.createElement("tr")
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+  
+    let text1 = document.createTextNode(attHigh10[i].first_name + " " + (attHigh10[i].middle_name || "") + " " + attHigh10[i].last_name);
+    link.appendChild(text1);
+  
+    let text2 = document.createTextNode(attHigh10[i].missed_votes);
+    let text3 = document.createTextNode(attHigh10[i].missed_votes_pct);
+  
+    
+    td1.appendChild(link);
+    td2.appendChild(text2);
+    td3.appendChild(text3);
+  
+    
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+  
+  
+  tblBody.appendChild(tr);
+  
+    }
+  
+    table.appendChild(thead);
+    table.appendChild(tblBody);
+  }
+  
+  buildTable4(attHigh10);
+  
