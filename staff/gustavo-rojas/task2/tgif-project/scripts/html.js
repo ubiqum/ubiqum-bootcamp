01364659b9b3de5bde155d559b62ba5bf66f9a72
Gustavo Rojas <grojas@fastmail.com>
2021-10-19 16:49:49 -0600

    Course2 task2 fetchJSON with array

--- staff/gustavo-rojas/task2/tgif-project/scripts/html.js
+++ staff/gustavo-rojas/task2/tgif-project/scripts/html.js
@@ -1,17 +1,35 @@
 let url = 'https://api.propublica.org/congress/v1/117/senate/members.json';
 var keyvalue = 'gdT8JyXvej1ZEHgazl8N6EvfRX88z8ik4qymrZ23'
 
+jsonsenators =[]
 function fetchJson(url, keyvalue) {
-    return fetch(url, {
-        mode: 'cors',
-        headers: {
-            'X-API-Key': keyvalue,
-            'Accept': '*/*',
-        },
-    })
-        .then(response => response.json())
+    return fetch(url, { mode: 'cors', headers: {'X-API-Key': keyvalue,'Accept': 'application/json', }, }).then(
+(value)=> {
+	return value.json()
+}
+).then(
+	(value) => {
+		jsonsenators=value
+	}
+)
         .catch(error => console.log('Error while fetching:', error))
 }
-function makeMemberRows(obj) {
+function makeMemberRows() {
+    member_senate_117 = fetchJson(url, keyvalue);
+    insert_row('senators-list',1,"Name","Party","State","Years in Office","Votes with Party");
+}
+function insert_row (table_id,first_row,name_text,party_text,state_text,yearinoffice_text,pervoteswithparty_text) {
+    var x =document.getElementById(table_id).insertRow(first_row);
+    var name = x.insertCell(0);
+    var party = x.insertCell(1);
+    var state = x.insertCell(2);
+    var yearinoffice = x.insertCell(3);
+    var pervoteswithparty = x.insertCell(4);
+    
+    name.innerHTML=name_text;
+    party.innerHTML=party_text;
+    state.innerHTML=state_text;
+    yearinoffice.innerHTML=yearinoffice_text;
+    pervoteswithparty.innerHTML=pervoteswithparty_text;
 
 }
