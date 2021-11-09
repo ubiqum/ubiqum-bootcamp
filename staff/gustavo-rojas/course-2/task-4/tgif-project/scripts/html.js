adb60e68b19d84851a1de00e33a2f90d6841eeb3
Gustavo Rojas <grojas@fastmail.com>
2021-11-09 11:08:23 -0600

    Course2 Task4 First release some refactoring of the code.

--- staff/gustavo-rojas/course-2/task-4/tgif-project/scripts/html.js
+++ staff/gustavo-rojas/course-2/task-4/tgif-project/scripts/html.js
@@ -274,6 +274,74 @@ if (actualpage === 'members.html') {
     waitForjsonsenators();
 }
 
+function atglancestats() {
+    members = jsonsenators.results[0].members;
+    const republicanssymbol = 'R';
+    var republicans = members.filter(x => x.party === republicanssymbol);
+    const democratsssymbol = 'D';
+    var democrats = members.filter(x => x.party === democratsssymbol);
+    const independentsymbol = 'ID';
+    var independent = members.filter(x => x.party === independentsymbol);
+    republicanswithvote = republicans.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
+    democratswithvote = democrats.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
+    independentwithvote = independent.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
+    rep_avg_votes_with_party_pct = republicanswithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / republicanswithvote.length;
+    if(Number.isNaN(rep_avg_votes_with_party_pct)) {
+        rep_avg_votes_with_party_pct= 'N/A';
+    } else { 
+        rep_avg_votes_with_party_pct=Number(rep_avg_votes_with_party_pct.toFixed(2));
+ 
+        };
+
+    dem_avg_votes_with_party_pct = democratswithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / democratswithvote.length;
+    if(Number.isNaN(dem_avg_votes_with_party_pct)) {
+        dem_avg_votes_with_party_pct='N/A';
+    } else {
+        dem_avg_votes_with_party_pct=Number(dem_avg_votes_with_party_pct.toFixed(2));
+ 
+ 
+    };
+    ind_avg_votes_with_party_pct = independentwithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / independentwithvote.length;
+    if(Number.isNaN(ind_avg_votes_with_party_pct)) {
+        ind_avg_votes_with_party_pct='N/A';
+    } else {
+            ind_avg_votes_with_party_pct=Number(ind_avg_votes_with_party_pct.toFixed(2));
+ 
+        }
+    var atglancestatsarray = [];
+    arrayaverage_totals=[rep_avg_votes_with_party_pct, dem_avg_votes_with_party_pct, ind_avg_votes_with_party_pct];
+    average_per_total= getaveragearray(arrayaverage_totals);
+    
+    if(Number.isNaN(average_per_total)) {
+        ind_avg_votes_with_party_pct='N/A';
+    } else {
+        average_per_total=Number(average_per_total.toFixed(2));
+ 
+        }
+ 
+    var array_total = ["Total", members.length,average_per_total];
+    atglancestatsarray.push(array_total);
+    var array_row3 = ["Independent", independent.length, ind_avg_votes_with_party_pct];
+    atglancestatsarray.push(array_row3);
+    var array_row2 = ["Democrats", democrats.length, dem_avg_votes_with_party_pct];
+    atglancestatsarray.push(array_row2);
+    var array_row1 = ["Republicans", republicans.length, rep_avg_votes_with_party_pct];
+    atglancestatsarray.push(array_row1);
+    return atglancestatsarray;
+}
+
+function getaveragearray (arrayoftotals) {
+    var only_numbers = arrayoftotals.filter(function (item) {
+        return (!isNaN(item));
+      });
+    if(only_numbers.length===0) {
+        return 'N/A'
+    };
+    average_number=only_numbers.reduce((a, b) => a + b, 0) / only_numbers.length;
+    return average_number;
+}
+
+
 
 if (actualpage === 'attendance.html') {
     var params = (new URL(document.location)).searchParams;
@@ -332,73 +400,8 @@ if (actualpage === 'attendance.html') {
 
     // I need to reproduce this in the other page in loyaly.html
 
-    function atglancestats() {
-        members = jsonsenators.results[0].members;
-        const republicanssymbol = 'R';
-        var republicans = members.filter(x => x.party === republicanssymbol);
-        const democratsssymbol = 'D';
-        var democrats = members.filter(x => x.party === democratsssymbol);
-        const independentsymbol = 'ID';
-        var independent = members.filter(x => x.party === independentsymbol);
-        republicanswithvote = republicans.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
-        democratswithvote = democrats.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
-        independentwithvote = independent.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
-        rep_avg_votes_with_party_pct = republicanswithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / republicanswithvote.length;
-        if(Number.isNaN(rep_avg_votes_with_party_pct)) {
-            rep_avg_votes_with_party_pct= 'N/A';
-        } else { 
-            rep_avg_votes_with_party_pct=Number(rep_avg_votes_with_party_pct.toFixed(2));
-     
-            };
+   
 
-        dem_avg_votes_with_party_pct = democratswithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / democratswithvote.length;
-        if(Number.isNaN(dem_avg_votes_with_party_pct)) {
-            dem_avg_votes_with_party_pct='N/A';
-        } else {
-            dem_avg_votes_with_party_pct=Number(dem_avg_votes_with_party_pct.toFixed(2));
-     
-     
-        };
-        ind_avg_votes_with_party_pct = independentwithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / independentwithvote.length;
-        if(Number.isNaN(ind_avg_votes_with_party_pct)) {
-            ind_avg_votes_with_party_pct='N/A';
-        } else {
-                ind_avg_votes_with_party_pct=Number(ind_avg_votes_with_party_pct.toFixed(2));
-     
-            }
-        var atglancestatsarray = [];
-        arrayaverage_totals=[rep_avg_votes_with_party_pct, dem_avg_votes_with_party_pct, ind_avg_votes_with_party_pct];
-        average_per_total= getaveragearray(arrayaverage_totals);
-        
-        if(Number.isNaN(average_per_total)) {
-            ind_avg_votes_with_party_pct='N/A';
-        } else {
-            average_per_total=Number(average_per_total.toFixed(2));
-     
-            }
-     
-        var array_total = ["Total", members.length,average_per_total];
-        atglancestatsarray.push(array_total);
-        var array_row3 = ["Independent", independent.length, ind_avg_votes_with_party_pct];
-        atglancestatsarray.push(array_row3);
-        var array_row2 = ["Democrats", democrats.length, dem_avg_votes_with_party_pct];
-        atglancestatsarray.push(array_row2);
-        var array_row1 = ["Republicans", republicans.length, rep_avg_votes_with_party_pct];
-        atglancestatsarray.push(array_row1);
-        return atglancestatsarray;
-    }
-
-    
-    function getaveragearray (arrayoftotals) {
-        var only_numbers = arrayoftotals.filter(function (item) {
-            return (!isNaN(item));
-          });
-        if(only_numbers.length===0) {
-            return 'N/A'
-        };
-        average_number=only_numbers.reduce((a, b) => a + b, 0) / only_numbers.length;
-        return average_number;
-    }
     function leastengaged_stats(percent) {
         members = jsonsenators.results[0].members;
         n_rows = (members.length * (percent / 100)).toFixed();
@@ -558,73 +561,7 @@ if (actualpage === 'loyalty.html') {
         }
 
     }
-    function atglancestats() {
-        members = jsonsenators.results[0].members;
-        const republicanssymbol = 'R';
-        var republicans = members.filter(x => x.party === republicanssymbol);
-        const democratsssymbol = 'D';
-        var democrats = members.filter(x => x.party === democratsssymbol);
-        const independentsymbol = 'ID';
-        var independent = members.filter(x => x.party === independentsymbol);
-        republicanswithvote = republicans.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
-        democratswithvote = democrats.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
-        independentwithvote = independent.filter(x => typeof (x.votes_with_party_pct) !== "undefined");
-        rep_avg_votes_with_party_pct = republicanswithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / republicanswithvote.length;
-        if(Number.isNaN(rep_avg_votes_with_party_pct)) {
-            rep_avg_votes_with_party_pct= 'N/A';
-        } else { 
-            rep_avg_votes_with_party_pct=Number(rep_avg_votes_with_party_pct.toFixed(2));
-     
-            };
-
-        dem_avg_votes_with_party_pct = democratswithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / democratswithvote.length;
-        if(Number.isNaN(dem_avg_votes_with_party_pct)) {
-            dem_avg_votes_with_party_pct='N/A';
-        } else {
-            dem_avg_votes_with_party_pct=Number(dem_avg_votes_with_party_pct.toFixed(2));
-     
-     
-        };
-        ind_avg_votes_with_party_pct = independentwithvote.reduce((a, b) => a + b.votes_with_party_pct, 0) / independentwithvote.length;
-        if(Number.isNaN(ind_avg_votes_with_party_pct)) {
-            ind_avg_votes_with_party_pct='N/A';
-        } else {
-                ind_avg_votes_with_party_pct=Number(ind_avg_votes_with_party_pct.toFixed(2));
-     
-            }
-        var atglancestatsarray = [];
-        arrayaverage_totals=[rep_avg_votes_with_party_pct, dem_avg_votes_with_party_pct, ind_avg_votes_with_party_pct];
-        average_per_total= getaveragearray(arrayaverage_totals);
-        
-        if(Number.isNaN(average_per_total)) {
-            ind_avg_votes_with_party_pct='N/A';
-        } else {
-            average_per_total=Number(average_per_total.toFixed(2));
-     
-            }
-     
-        var array_total = ["Total", members.length,average_per_total];
-        atglancestatsarray.push(array_total);
-        var array_row3 = ["Independent", independent.length, ind_avg_votes_with_party_pct];
-        atglancestatsarray.push(array_row3);
-        var array_row2 = ["Democrats", democrats.length, dem_avg_votes_with_party_pct];
-        atglancestatsarray.push(array_row2);
-        var array_row1 = ["Republicans", republicans.length, rep_avg_votes_with_party_pct];
-        atglancestatsarray.push(array_row1);
-        return atglancestatsarray;
-    }
-    function getaveragearray (arrayoftotals) {
-        var only_numbers = arrayoftotals.filter(function (item) {
-            return (!isNaN(item));
-          });
-        if(only_numbers.length===0) {
-            return 'N/A'
-        };
-        average_number=only_numbers.reduce((a, b) => a + b, 0) / only_numbers.length;
-        return average_number;
-    }
-
-
+    
     function leastloyal_stats(percent) {
         members = jsonsenators.results[0].members;
         n_rows = (members.length * (percent / 100)).toFixed();
