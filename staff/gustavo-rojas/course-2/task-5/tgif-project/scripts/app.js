9670a634412ab32119655c6022a559cab32d0133
Gustavo Rojas <grojas@fastmail.com>
2021-11-11 12:51:36 -0600

    Course2 Task5 First Vue Code

--- staff/gustavo-rojas/course-2/task-5/tgif-project/scripts/app.js
+++ staff/gustavo-rojas/course-2/task-5/tgif-project/scripts/app.js
@@ -1,9 +1,29 @@
 const app = Vue.createApp({
   data() {
     return { 
-      chamber: 'senate'
+      chamber: this.getChamber()
+    }
+  },
+  methods: {
+    getChamber() {
+      var params = (new URL(document.location)).searchParams;
+      var chamber_params = params.get('chamber');
+      var chamber="";
+      switch (chamber_params) {
+        case 'senate':
+            var chamber = 'senate'
+            break;
+        case 'house':
+           var chamber = 'house'
+            break;
+        case null:
+            var chamber = 'senate'
+            break;
+        default:
+              var chamber = 'senate'
+    }
+     return chamber
     }
   }
 });
-
-app.mount('#app');
+  app.mount('#app');
