c508df052eb870ac8ac86993367c64ae0bff5a67
Gustavo Rojas <grojas@fastmail.com>
2021-11-24 14:23:03 -0600

    3. Add styling

--- staff/gustavo-rojas/course-3/task-2/scheduler/src/App.js
+++ staff/gustavo-rojas/course-3/task-2/scheduler/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import './App.css';
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
 
 const CourseList = ({ courses }) => (
-  <div>
-    { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+  <div className="course-list">
+  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
   </div>
 );
 
@@ -54,7 +58,7 @@ const schedule = {
 };
 
 const App = () =>  (
-  <div>
+  <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
