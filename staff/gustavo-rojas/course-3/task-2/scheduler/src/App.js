a28d5d41ff53c1f61d7e1580c24aa5c89a518c34
Gustavo Rojas <grojas@fastmail.com>
2021-11-24 16:55:15 -0600

    3. Add styling

--- staff/gustavo-rojas/course-3/task-2/scheduler/src/App.js
+++ staff/gustavo-rojas/course-3/task-2/scheduler/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-// import React from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 const Banner = ({ title }) => (
