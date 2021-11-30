1afe79038526d53afc695d609c6e14dde651a34f
Gustavo Rojas <grojas@fastmail.com>
2021-11-29 22:42:42 -0600

    8. Read and write to a database

--- staff/gustavo-rojas/course-3/task-2/scheduler/src/App.js
+++ staff/gustavo-rojas/course-3/task-2/scheduler/src/App.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
-// import React from 'react';
 import './App.css';
-// import { hasConflict,getCourseTerm,getCourseNumber,terms } from './utilities/times.js';
 import CourseList from './components/CourseList';
-
+import { useData } from './utilities/firebase.js';
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
-
 
 const addScheduleTimes = schedule => ({
   title: schedule.title,
@@ -36,28 +33,12 @@ const addCourseTimes = course => ({
 });
 
 
-
-
-
-
-
-
-
-
 const App = () => {
-  const [schedule, setSchedule] = useState();
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  const [schedule, loading, error] = useData('/', addScheduleTimes); 
+  
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading the schedule...</h1>
 
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(addScheduleTimes(json));
-    }
-    fetchSchedule();
-  }, [])
-  if (!schedule) return <h1>Loading schedule...</h1>; // This line is missing in 6. Filter out course conflicts
   return (
     <div className="container">
       <Banner title={ schedule.title } />
---
+++ staff/gustavo-rojas/course-3/task-2/scheduler/src/utilities/firebase.js
@@ -0,0 +1,42 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref } from 'firebase/database';
+import { useState, useEffect } from 'react';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzVKdflz2JigP0AWmWAKX-VM4OlmyXxAc",
+  authDomain: "scheduler-637fd.firebaseapp.com",
+  databaseURL: "https://scheduler-637fd-default-rtdb.firebaseio.com",
+  projectId: "scheduler-637fd",
+  storageBucket: "scheduler-637fd.appspot.com",
+  messagingSenderId: "7304756838",
+  appId: "1:7304756838:web:b5144b00ed4b3e493af7a7",
+  measurementId: "G-BSXHPFCRQD"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const dbRef = ref(database, path);
+    const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+    if (devMode) { console.log(`loading ${path}`); }
+    return onValue(dbRef, (snapshot) => {
+      const val = snapshot.val();
+      if (devMode) { console.log(val); }
+      setData(transform ? transform(val) : val);
+      setLoading(false);
+      setError(null);
+    }, (error) => {
+      setData(null);
+      setLoading(false);
+      setError(error);
+    });
+  }, [path, transform]);
+
+  return [data, loading, error];
+};
