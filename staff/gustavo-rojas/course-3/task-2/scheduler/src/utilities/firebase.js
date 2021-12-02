0f928ea828532295a018adb203cf9aee702037f4
Gustavo Rojas <grojas@fastmail.com>
2021-12-01 21:37:50 -0600

    8. Read and write to a database

--- staff/gustavo-rojas/course-3/task-2/scheduler/src/utilities/firebase.js
+++ staff/gustavo-rojas/course-3/task-2/scheduler/src/utilities/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, onValue, ref } from 'firebase/database';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
 import { useState, useEffect } from 'react';
 
 const firebaseConfig = {
