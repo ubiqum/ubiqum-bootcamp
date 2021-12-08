f3a974442f2af246a304ccdab99f11903877278f
Gustavo Rojas <grojas@fastmail.com>
2021-12-07 21:48:23 -0600

    Added the router and set the homepage

--- staff/gustavo-rojas/course-3/task-3/site-nysl/src/App.js
+++ staff/gustavo-rojas/course-3/task-3/site-nysl/src/App.js
@@ -2,22 +2,68 @@ import React from 'react';
 import logo from './nysl_logo.svg';
 import './App.css';
 import {nysl_league,sport_events} from "./components/home.js"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div>
-    
-    <h5 > <img src={logo} alt="Logo NYSL" width="100" /> { nysl_league.title }</h5>
-    <ul>
-        {sport_events.map(sport_event => {
-          return (
-          <li>{sport_event.s_event_date} - {sport_event.s_event_title}</li>
-          )
-        })}
-      </ul>
-  </div>
-   
+    <Router>
+      <div>
+        
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <div>
+    
+  <h5 > <img src={logo} alt="Logo NYSL" width="100" /> { nysl_league.title }</h5>
+  <ul>
+      {sport_events.map(sport_event => {
+        return (
+        <li className="list-group-item">{sport_event.s_event_date} - {sport_event.s_event_title}</li>
+        )
+      })}
+    </ul>
+</div>
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ staff/gustavo-rojas/course-3/task-3/site-nysl/src/components/gameinfo.js
@@ -0,0 +1,112 @@
+export const game_info = {
+    "2018-01-09": [
+       {
+          "Teams": "U1 and U4",
+          "Location": "AJ Katzenmaier",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U3 and U2",
+          "Location": "Greenbay",
+          "Times": "1:00 p.m."
+       }
+    ],
+    "2018-08-09": [
+       {
+          "Teams": "U5 and U6",
+          "Location": "Howard A Yeager",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U6 and U1",
+          "Location": "Marjorie P Hart",
+          "Times": "1:00 p.m."
+       }
+    ],
+    "2018-15-09": [
+       {
+          "Teams": "U2 and U4",
+          "Location": "North",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U3 and U5",
+          "Location": "AJ Katzenmaier",
+          "Times": "1:00 p.m."
+       }
+    ],
+    "2018-22-09": [
+       {
+          "Teams": "U1 and U3",
+          "Location": "South",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U2 and U6",
+          "Location": "Howard A Yeager",
+          "Times": "1:00 p.m."
+       }
+    ],
+    "2018-29-09": [
+       {
+          "Teams": "U4 and U5",
+          "Location": "Greenbay",
+          "Times": "9:30 a.m."
+       }
+    ],
+    "Date": [
+       {
+          "Teams": "Teams",
+          "Location": "Location",
+          "Times": "Times"
+       }
+    ],
+    "2018-06-10": [
+       {
+          "Teams": "U2 and U5",
+          "Location": "Marjorie P Hart",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U1 and U6",
+          "Location": "South",
+          "Times": "1:00 p.m."
+       }
+    ],
+    "2018-13-10": [
+       {
+          "Teams": "U3 and U4",
+          "Location": "Howard A Yeager",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U5 and U1",
+          "Location": "Greenbay",
+          "Times": "1:00 p.m."
+       }
+    ],
+    "2018-20-10": [
+       {
+          "Teams": "U6 and U3",
+          "Location": "North",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U2 and U4",
+          "Location": "Marjorie P Hart",
+          "Times": "1:00 p.m."
+       }
+    ],
+    "2018-27-10": [
+       {
+          "Teams": "U3 and U1",
+          "Location": "AJ Katzenmaier",
+          "Times": "9:30 a.m."
+       },
+       {
+          "Teams": "U5 and U6",
+          "Location": "Howard A Yeager",
+          "Times": "1:00 p.m."
+       }
+    ]
+ }
