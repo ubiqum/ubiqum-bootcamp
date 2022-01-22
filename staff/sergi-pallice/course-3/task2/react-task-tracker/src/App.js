6568574d76eaca3e8756791cf88c67914a60019e
Sergi Pallice <spallice@gmail.com>
2022-01-22 16:54:07 +0100

    added new components

--- staff/sergi-pallice/course-3/task2/react-task-tracker/src/App.js
+++ staff/sergi-pallice/course-3/task2/react-task-tracker/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import { useState } from 'react'
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
-  const name = "Sergi";
-
-  return (
+  const name = "Task Tracker";
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th, 14:30'
+        },
+        {
+            id: 2,
+            text: 'Work Meeting',
+            day: 'Feb 6th, 10:30'  
+        }
+    ])
+    //Delete Task
+    const deleteTask = (id) => {
+      console.log('delete task id:', id)
+      setTasks(tasks.filter( (task) => task.id !== id ))
+    }
+    return (
     <div className="container">
       <Header title={name}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
     </div>
   );
 }
--- staff/sergi-pallice/course-3/task2/react-task-tracker/src/components/Header.js
+++ staff/sergi-pallice/course-3/task2/react-task-tracker/src/components/Header.js
@@ -1,8 +1,15 @@
-const Header = ({title}) => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('clicked button')
+  } 
+
   return (
     <header className='header'>
       <h1 style={headingStyle}>{title}</h1>
-      <button className='btn'>Add</button>
+      <Button text='Add' onClick={onClick}/>
     </header>
   )
 }
@@ -11,10 +18,14 @@ Header.defaultProps = {
     title: "Task Tracker"
 }
 
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
 // CSS in JS
 const headingStyle = {
-    color: 'red', 
-    backgroundColor: 'black' 
+    //color: 'red', 
+    //backgroundColor: 'black' 
 }
 
 export default Header
