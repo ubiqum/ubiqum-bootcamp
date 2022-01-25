cca48cd42265059b0626ee705e54d1a2c3e514e1
Sergi Pallice <spallice@gmail.com>
2022-01-25 21:18:34 +0100

    setting up firebase

--- staff/sergi-pallice/course-3/task2/scheduler/src/App.js
+++ staff/sergi-pallice/course-3/task2/scheduler/src/App.js
@@ -1,6 +1,21 @@
 //import Banner from './components/Banner'
 //import CourseList from './components/CourseList';
 import { useState, useEffect } from 'react'
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "....",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
 
 //App link: https://ubiqum-react-scheduler.web.app/
 
