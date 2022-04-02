027311a43049acee407b77280de09d6415364684
JakubGaba <j.gaba@email.cz>
2022-04-02 17:57:00 +0200

    Just for show on other PC

--- staff/jakub-gaba/course-2/task-2/tgif-project/scripts/html.js
+++ staff/jakub-gaba/course-2/task-2/tgif-project/scripts/html.js
@@ -1,20 +1,39 @@
 
 var API = 'werjoImcxnptmekG4oOVTHidgbQOBbmJh4gj90mn';
 var url = 'https://api.propublica.org/congress/v1/117/senate/members.json';
-var urlHouse= 'https://api.propublica.org/congress/v1/117/house/members.json';
+var urlHouse = 'https://api.propublica.org/congress/v1/117/house/members.json';
 var D = [];
 var ID = [];
 var R = [];
 var congressData;
+var houseData;
 var Republicans = document.querySelector("input[id=republicanscheckbox]");
 var Democrats = document.querySelector("input[id=democratscheckbox]");
 var Independent = document.querySelector("input[id=independentcheckbox]");
 var addStates = document.getElementById("states_add");
 document.getElementById("states_add").onchange = function () { changing() };
+var fickDich = document.getElementById("fick-dich");
 //curl --request GET https://api.propublica.org/congress/v1/117/house/members.json --header "X-Api-Key:werjoImcxnptmekG4oOVTHidgbQOBbmJh4gj90mn"
 
 
+fickDich.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (evt.target.innerHTML == "HOUSE") {
+    console.log(evt.target.innerHTML);
+  }
+  else {
+    let url = new URL("http://127.0.0.1:5500/jakub-gaba/course-2/task-2/tgif-project/Members.html");
+    let params = new URLSearchParams(url.search);
+    params.append("Chamber", "Senate");
+
+    document.getElementsByClassName("container")[0].style.display = "";
+    console.log("You choose Senate");
+  }
+});
+
+
 export async function fetchJson() {            //Fetching data from JSON
+
   const response = await fetch(url, {
     method: "GET",
     headers: {
@@ -25,15 +44,15 @@ export async function fetchJson() {            //Fetching data from JSON
   return data;
 }
 
-export async function fetchJsonHouse() {            //Fetching data from JSON
+export async function fetchJsonHouse() {            //Fetching data from JSON house
   const responseHouse = await fetch(urlHouse, {
     method: "GET",
     headers: {
       "X-API-Key": API
     }
   });
-  const dataHouse = await response.json();
-  return dataHouse;
+  const data = await response.json();
+  return data;
 }
 
 
@@ -49,7 +68,6 @@ export function makeMemberRows(data) {
   filterByParty(data);
   makeStatesMenu();
 }
-
 
 // ----------- Pushing and sorting to an array Republicans, Democrats, Indepentent groups. -------------
 export function filterByParty(data) {
@@ -67,15 +85,14 @@ export function filterByParty(data) {
 
 }
 
-
 // -------------  Creates menu for states.   -------------
-export async function showStateData() {              
+export async function showStateData() {
   const response = await fetch("./states.json");
   const data = await response.json();
   return data;
 }
 
-export function makeStatesMenu() {           
+export function makeStatesMenu() {
   showStateData().then(data => {
     const keys = Object.keys(data);
     const value = Object.values(data);
@@ -90,7 +107,7 @@ export function makeStatesMenu() {
 
 
 // -------------   State filter for changing   -------------
-function changing() {                               
+function changing() {
   if (Republicans.checked || Democrats.checked || Independent.checked) {
     filterStateChanger();
   }
@@ -101,66 +118,66 @@ function changing() {
   }
 }
 
-function filterStateChanger(){
+function filterStateChanger() {
   var stateRep = "statesChangeRep";
-    var stateDem = "statesChangeDem";
-    var stateInd = "statesChangeInd";
-    document.getElementById("R").innerHTML = "";
-    document.getElementById("D").innerHTML = "";
-    document.getElementById("ID").innerHTML = "";
-    document.getElementById("statesChangeRep").innerHTML = "";
-    document.getElementById("statesChangeDem").innerHTML = "";
-    document.getElementById("statesChangeInd").innerHTML = "";
-    for (let i = 0; i <= congressData.results[0].members.length - 1; i++) {
-      if (congressData.results[0].members[i].state.includes(document.getElementById("states_add").value) == true) { //porovnava jestli stat na indexu obsahuje stat zadanej
-        if ((congressData.results[0].members[i].party == "R") && (Republicans.checked)) {
-          document.getElementById("R").innerHTML = "";
+  var stateDem = "statesChangeDem";
+  var stateInd = "statesChangeInd";
+  document.getElementById("R").innerHTML = "";
+  document.getElementById("D").innerHTML = "";
+  document.getElementById("ID").innerHTML = "";
+  document.getElementById("statesChangeRep").innerHTML = "";
+  document.getElementById("statesChangeDem").innerHTML = "";
+  document.getElementById("statesChangeInd").innerHTML = "";
+  for (let i = 0; i <= congressData.results[0].members.length - 1; i++) {
+    if (congressData.results[0].members[i].state.includes(document.getElementById("states_add").value) == true) { //porovnava jestli stat na indexu obsahuje stat zadanej
+      if ((congressData.results[0].members[i].party == "R") && (Republicans.checked)) {
+        document.getElementById("R").innerHTML = "";
+        writeToHTML(stateRep, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
+      }
+      else if ((congressData.results[0].members[i].party == "D") && (Democrats.checked)) {
+        document.getElementById("D").innerHTML = "";
+        writeToHTML(stateDem, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
+      }
+      else if ((congressData.results[0].members[i].party == "ID") && (Independent.checked)) {
+        document.getElementById("ID").innerHTML = "";
+        writeToHTML(stateInd, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
+      }
+      else if ((congressData.results[0].members[i].party == "R") && (congressData.results[0].members[i].party == "D") && (Democrats.checked) && (Republicans.checked)) {
+        document.getElementById("D").innerHTML = "";
+        document.getElementById("R").innerHTML = "";
+        if (congressData.results[0].members[i].party == "R") {
           writeToHTML(stateRep, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
         }
-        else if ((congressData.results[0].members[i].party == "D") && (Democrats.checked)) {
-          document.getElementById("D").innerHTML = "";
+        else {
           writeToHTML(stateDem, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
         }
-        else if ((congressData.results[0].members[i].party == "ID") && (Independent.checked)) {
-          document.getElementById("ID").innerHTML = "";
-          writeToHTML(stateInd, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
-        }
-        else if ((congressData.results[0].members[i].party == "R") && (congressData.results[0].members[i].party == "D") && (Democrats.checked) && (Republicans.checked)) {
-          document.getElementById("D").innerHTML = "";
-          document.getElementById("R").innerHTML = "";
-          if (congressData.results[0].members[i].party == "R") {
-            writeToHTML(stateRep, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
-          }
-          else {
-            writeToHTML(stateDem, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
-          }
-         }
-        else if ((congressData.results[0].members[i].party == "R") && (congressData.results[0].members[i].party == "ID") && (Independent.checked) && (Republicans.checked)) {
-          document.getElementById("ID").innerHTML = "";
-          document.getElementById("R").innerHTML = "";
-          if (congressData.results[0].members[i].party == "R") {
-            writeToHTML(stateRep, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
-          }
-          else {
-            writeToHTML(stateInd, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
-          }
-        }
-        else if ((congressData.results[0].members[i].party == "D") && (congressData.results[0].members[i].party == "ID") && (Independent.checked) && (Democrats.checked)) {
-          document.getElementById("D").innerHTML = "";
-          document.getElementById("ID").innerHTML = "";
-          if (congressData.results[0].members[i].party == "D") {
-            writeToHTML(stateDem, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
-          }
-          else {
-            writeToHTML(stateInd, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
-          }
-
+      }
+      else if ((congressData.results[0].members[i].party == "R") && (congressData.results[0].members[i].party == "ID") && (Independent.checked) && (Republicans.checked)) {
+        document.getElementById("ID").innerHTML = "";
+        document.getElementById("R").innerHTML = "";
+        if (congressData.results[0].members[i].party == "R") {
+          writeToHTML(stateRep, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
         }
         else {
-         
+          writeToHTML(stateInd, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
         }
       }
+      else if ((congressData.results[0].members[i].party == "D") && (congressData.results[0].members[i].party == "ID") && (Independent.checked) && (Democrats.checked)) {
+        document.getElementById("D").innerHTML = "";
+        document.getElementById("ID").innerHTML = "";
+        if (congressData.results[0].members[i].party == "D") {
+          writeToHTML(stateDem, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
+        }
+        else {
+          writeToHTML(stateInd, congressData.results[0].members[i].first_name, congressData.results[0].members[i].last_name, congressData.results[0].members[i].party);
+        }
+
+      }
+      else {
+
+      }
     }
+  }
 }
 function writeToHTML(idBro, nameBud, lastNameBud, partyBro) {
   var row = document.getElementById(idBro).insertRow();
